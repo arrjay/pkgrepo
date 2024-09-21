2fbb9a952650c8458ca92ed88cceb19016f8a25e
RJ Bergeron <rj@bergeron-us.net>
2024-09-21 01:58:04 +0000

    this makes a workable layout, need to sort out signing

--- .github/workflows/generate-repositories.sh
+++ .github/workflows/generate-repositories.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 set -eux
 
-DEB_TOP="_site/deb"
+SITE_TOP="$(pwd)/_site"
+POOL_TOP="${SITE_TOP}/pool"
+DIST_TOP="${SITE_TOP}/dists"
+
+# from https://github.com/terminate-notice/terminate-notice.github.io/blob/main/.github/scripts/build_repos.sh
+generate_hashes() {
+  HASH_TYPE="$1"
+  HASH_COMMAND="$2"
+  echo "${HASH_TYPE}:"
+  find "${COMPONENTS:-main}" -type f | while read -r file
+  do
+    echo " $(${HASH_COMMAND} "$file" | cut -d" " -f1) $(wc -c "$file")"
+  done
+}
 
 grep -v '^#' < .github/config/repositories.txt | while IFS= read -r repo ; do
   version="${repo#*-}" ; distro="${repo%-*}"
@@ -9,7 +22,7 @@ grep -v '^#' < .github/config/repositories.txt | while IFS= read -r repo ; do
     tag="${release#*:}" repo="${release%:*}" ;
     releasejson="/tmp/${release}.json"
     assetfile="${repo}-${distro}-${version}.zip" ; assetpath="/tmp/${assetfile}"
-    debout="${DEB_TOP}/pool/${distro}-${version}"
+    debout="${POOL_TOP}/${distro}-${version}"
     [[ -e "${releasejson}" ]] || curl -L -o "${releasejson}" "https://api.github.com/repos/arrjay/${repo}/releases/tags/${tag}"
     asset_url="$(jq -r '.assets[] | select(.name=="'"${assetfile}"'").url' < "${releasejson}")"
     [[ -e "${assetpath}" ]] || curl -L -H 'Accept: application/octet-stream' -o "${assetpath}" "${asset_url}"
@@ -18,17 +31,29 @@ grep -v '^#' < .github/config/repositories.txt | while IFS= read -r repo ; do
   done
 done
 
-for ent in "${DEB_TOP}/pool"/* ; do
-  distdir="${DEB_TOP}/${ent##*/}"
-  mkdir -p "${distdir}"
-  dpkg-scanpackages "${ent}/" > "${distdir}/Packages"
-  gzip -9 > "${distdir}/Packages.gz" < "${distdir}/Packages"
-  bzip2 -9 > "${distdir}/Packages.bz2" < "${distdir}/Packages"
+for ent in "${POOL_TOP}"/* ; do
+  distdir="${DIST_TOP}/${ent##*-}"
+  for bin in all amd64 ; do
+    mkdir -p "${distdir}/main/binary-${bin}"
+    pushd "${SITE_TOP}" >/dev/null 2>&1
+      dpkg-scanpackages -a "${bin}" ".${ent#"${SITE_TOP}"}/" > "${distdir}/main/binary-${bin}/Packages"
+    popd >/dev/null 2>&1
+    gzip -9 > "${distdir}/main/binary-${bin}/Packages.gz" < "${distdir}/main/binary-${bin}/Packages"
+    bzip2 -9 > "${distdir}/main/binary-${bin}/Packages.bz2" < "${distdir}/main/binary-${bin}/Packages"
+    xz -9 > "${distdir}/main/binary-${bin}/Packages.xz" < "${distdir}/main/binary-${bin}/Packages"
+    zstd > "${distdir}/main/binary-${bin}/Packages.zst" < "${distdir}/main/binary-${bin}/Packages"
+    lz4 -9 > "${distdir}/main/binary-${bin}/Pazkages.lz4" < "${distdir}/main/binary-${bin}/Packages"
+  done
   {
     printf '%s: %s\n' \
       Suite releases \
-      Codename "${ent##*-}" \
       Components main \
+      Codename "${ent##*-}" \
       Date "$(date -Ru)"
+      pushd "${distdir}" >/dev/null 2>&1
+      generate_hashes MD5Sum md5sum
+      generate_hashes SHA1 sha1sum
+      generate_hashes SHA256 sha256sum
+      popd >/dev/null 2>&1
   } > "${distdir}/Release"
 done
